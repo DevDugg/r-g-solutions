55da3ebde700b35f73d3286dd03e5f0f857f3c23
DevDugg <mr.aydogdy02@gmail.com>
2025-02-08 14:21:50 +0500

    section card correction

--- src/components/elements/section-card.tsx
+++ src/components/elements/section-card.tsx
@@ -8,7 +8,7 @@ interface SectionCardProps {
 const SectionCard = ({ title, description }: SectionCardProps) => {
   return (
     <div className="section-card bg-BG p-4 rounded-lg h-[240px] flex flex-col justify-between">
-      <div className="flex gap-4">
+      <div className="flex gap-4 justify-between">
         <h3 className="card-title text-BLACK">{title}</h3>
         <DecoratorCircle size={12} color={"BLACK"} />
       </div>
