1442a58afe1e1b9fdf8c2c324bb41ae0ebab7e95
DevDugg <mr.aydogdy02@gmail.com>
2025-02-16 19:16:30 +0500

    responsive benefits sections

--- src/components/elements/benefits-card.tsx
+++ src/components/elements/benefits-card.tsx
@@ -10,14 +10,14 @@ interface BenefitsCardProps {
 const BenefitsCard = ({ title, description, background, index }: BenefitsCardProps) => {
   return (
     <div
-      className="flex flex-col gap-6 p-4 rounded-lg text-BLACK TABLET:flex-row TABLET:justify-between TABLET:gap-12"
+      className="flex flex-col gap-6 p-4 rounded-lg text-BLACK overflow-hidden TABLET:flex-row TABLET:justify-between TABLET:gap-12 WIDE:flex-col WIDE:gap-28"
       style={{ background }}
     >
       <div className="flex flex-col gap-4 TABLET:justify-between">
-        <h3 className="card-title">{title}</h3>
+        <h3 className="card-title WIDE:max-w-[75%]">{title}</h3>
         <h4 className="body-text TABLET:max-w-56">{description}</h4>
       </div>
-      <span className="decor-text-1">{formatToDoubleDigits(index + 1)}</span>
+      <span className="decor-text-1 WIDE:-ml-16 DESKTOP:-ml-24">{formatToDoubleDigits(index + 1)}</span>
     </div>
   );
 };
--- src/components/sections/benefits.tsx
+++ src/components/sections/benefits.tsx
@@ -7,19 +7,20 @@ import benefits from "@/data/benefits";
 const Benefits = () => {
   return (
     <section id="benefits" className="benefits">
-      <Container className="flex flex-col gap-12 bg-BLACK rounded-2xl p-4 TABLET:p-6">
+      <Container className="flex flex-col gap-12 bg-BLACK rounded-2xl p-4 TABLET:p-6 WIDE:grid WIDE:grid-cols-[minmax(200px, 400px)_minmax(243px,759px)] WIDE:gap-x-[113px] WIDE:gap-y-6 DESKTOP:grid-cols-[400px_minmax(243px,759px)]">
         <div className="flex flex-col gap-4 text-WHITE">
           <div className="flex gap-2 items-center">
             <DecoratorCircle size={8} color={"WHITE"} /> <span className="tag-text">{benefits.label}</span>
           </div>
           <h2 className="section-title">{benefits.title}</h2>
         </div>
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 WIDE:flex-row WIDE:gap-4">
           {benefits.cards.map((card, i) => (
             <BenefitsCard {...card} index={i} key={card.title} />
           ))}
-          <Button className="w-full" label={benefits.cta.label} href={benefits.cta.href} />
+          <Button className="w-full WIDE:hidden" label={benefits.cta.label} href={benefits.cta.href} />
         </div>
+        <Button className="w-full hidden WIDE:flex col-span-2" label={benefits.cta.label} href={benefits.cta.href} />
       </Container>
     </section>
   );
